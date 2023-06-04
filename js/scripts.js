6d402bf9ca4db244c3fa1a07f8ceeeb4e14d798c
Aaron Demski <ajdemski@frontier.com>
2023-06-04 13:58:36 -0700

    this code should take and display the suggestion based on the users answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,16 @@
-//User Interface Logic
-
-function hideResults() {
-  document.getElementById("python").setAttribute("class", "hidden");
-  document.getElementById("c#").setAttribute("class", "hidden");
-  document.getElementById("javascript").setAttribute("class", "hidden");
-}
+// User Interface Logic
+document.getElementById("survey").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const answer1 = document.getElementById("answers-1").value;
+  const answer2 = document.getElementById("answers-2").value;
+  const answer3 = document.getElementById("answers-3").value;
+  const age = parseInt(document.getElementById("age").value);
+  const time = parseInt(document.getElementById("time").value);
 
 //Business Interface Logic
 
-window.onload = function () {
-  hideResults();
-
-  document.querySelector("form").onsubmit = function (event) {
-    event.preventDefault();
-    hideResults();
-    const answer1 = document.getElementById("answers-1").value;
-    const answer2 = document.getElementById("answers-2").value;
-    const answer3 = document.getElementById("answers-3").value;
-    let suggestion = "";
-
-    if (answer1 === "caesar" || answer2 === "Olympics" || answer3 === "offGrid") {
-      suggestion = "javascript";
-    } else if (answer1 === "earhart" || answer2 === "marsMission" || answer3 === "winter") {
-      suggestion = "c#";
-    } else (answer1 === "armstrong" || answer2 === "offGrid" || answer3 === "spring") 
-      suggestion = "python";
-    }}
+const suggestion = document.getElementById("suggestion");
+suggestion.textContent = generateSuggestion(answer1, answer2, answer3, age, time);
+document.getElementById("result").style.display = "block";
+});
