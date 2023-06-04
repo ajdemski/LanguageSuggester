ca4308fdec356e8457d70a63bd6167812d0aa40e
Aaron Demski <ajdemski@frontier.com>
2023-06-04 15:46:51 -0700

    IT WORKS! I found out that I needed to add a 'DOMContentLoaded' event listener so then my JavaScript will run after the HTML has finished loading, still have to fix a bunch of things and format the result better but the code works!

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 // User Interface Logic
-document.getElementById("survey").addEventListener("submit" , function(event) {
-  event.preventDefault();
 
-  const answer1 = document.getElementById("answers-1").value;
-  const answer2 = document.getElementById("answers-2").value;
-  const answer3 = document.getElementById("answers-3").value;
-  const age = parseInt(document.getElementById("age").value);
-  const time = parseInt(document.getElementById("time").value);
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("survey").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const suggestion = document.getElementById("suggestion");
-  suggestion.textContent = generateSuggestion(answer1, answer2, answer3, age, time);
-  document.getElementById("result").style.display = "block";
+    const answer1 = document.getElementById("answers-1").value;
+    const answer2 = document.getElementById("answers-2").value;
+    const answer3 = document.getElementById("answers-3").value;
+    const age = parseInt(document.getElementById("age").value);
+    const time = parseInt(document.getElementById("time").value);
+
+    const suggestion = document.getElementById("suggestion");
+    suggestion.textContent = generateSuggestion(answer1, answer2, answer3, age, time);
+    document.getElementById("result").style.display = "block";
+  });
+
+  //Business Interface Logic
+
+  function generateSuggestion(answer1, answer2, answer3, age, time) {
+    if (answer1 === "Olympics" && age >= 18 && time >= 4) {
+      return "You should learn Python!";
+    } else if (answer2 === "earhart" && age >= 18 && time >= 6) {
+      return "You should learn C#!";
+    } else {
+      return "You should learn JavaScript";
+    }
+  }
 });
-
-//Business Interface Logic
-
