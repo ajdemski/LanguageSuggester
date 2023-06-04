51b2f1c4a8d88a1b48025203d0d7588aa3109ebb
Aaron Demski <ajdemski@frontier.com>
2023-06-04 14:13:21 -0700

    getting an error, time to troubleshoot and I dont like my branching logic. it is very rare to get the output python and c# because of the 5 specific answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 // User Interface Logic
-document.getElementById("survey").addEventListener("submit", function(event) {
+document.getElementById("survey").addEventListener("submit", function (event) {
   event.preventDefault();
-  
+
   const answer1 = document.getElementById("answers-1").value;
   const answer2 = document.getElementById("answers-2").value;
   const answer3 = document.getElementById("answers-3").value;
   const age = parseInt(document.getElementById("age").value);
   const time = parseInt(document.getElementById("time").value);
 
-//Business Interface Logic
-
-const suggestion = document.getElementById("suggestion");
-suggestion.textContent = generateSuggestion(answer1, answer2, answer3, age, time);
-document.getElementById("result").style.display = "block";
+  const suggestion = document.getElementById("suggestion");
+  suggestion.textContent = generateSuggestion(answer1, answer2, answer3, age, time);
+  document.getElementById("result").style.display = "block";
 });
+
+//Business Interface Logic
+function generateSuggestion(answer1, answer2, answer3, age, time) {
+
+  if (answer1 === "Olympics" && answer2 === "caesar" && answer3 === "summer" && age >= 18 && time >= 4) {
+    return "You should learn Python!";
+  } else if (answer1 === "marsMission" && answer2 === "earhart" && answer3 === "winter" && age >= 18 && time >= 6) {
+    return "You should learn C#!";
+  } else {
+    return "You should learn JavaScript!";
+  }
+}
